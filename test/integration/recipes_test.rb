76c0b09cd1f69d3e3f118af7fbac67368a2ed8e8
Dan Bonehill <Danbonehill@me.com>
2017-04-01 11:20:11 +0000

    Add Show page & tests.

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -5,7 +5,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
   def setup
     # One way to create a Recipe
     @chef = Chef.create(chefname: "Dan", email: "dan@example.com")
-    @recipe = Recipe.create(name: "Chicken Salad", description: "Fresh salad with Chicken strips", chef: @chef)
+    @recipe = Recipe.create(name: "Chicken salad", description: "Fresh salad with Chicken strips", chef: @chef)
     # Second way to create a Recipe
     @recipe2 = @chef.recipes.build(name: "Steak and Chips", description: "Rump steak with chips")
     @recipe2.save
@@ -19,7 +19,15 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template "recipes/index"
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
+  end
+  
+  test "shoulld get recipes show" do
+    get recipe_path(@recipe)
+    assert_template "recipes/show"
     assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.chefname, response.body
   end
 end
