ca2826ff016e702457b0b63163153531c9cf7095
Dan Bonehill <Danbonehill@me.com>
2017-04-05 19:28:52 +0000

    Add chefs delete and test

--- test/models/chef_test.rb
+++ test/models/chef_test.rb
@@ -71,4 +71,12 @@ class ChefTest < ActiveSupport::TestCase
     assert_not @chef.valid?
   end
   
+  test "associated recipes should be destroyed" do
+    @chef.save
+    @chef.recipes.create!(name: "Testing Destroy", description: "testing destroy")
+    assert_difference "Recipe.count", -1 do
+      @chef.destroy
+    end
+  end
+  
 end
