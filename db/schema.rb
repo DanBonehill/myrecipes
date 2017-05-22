4a99ae84002709920ccb2ea63522223b2dcbcca8
Dan Bonehill <Danbonehill@me.com>
2017-05-22 18:16:30 +0000

    Add Ingredient & Recipe assocciation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522173042) do
+ActiveRecord::Schema.define(version: 20170522175555) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 20170522173042) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "submitted_by"
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "ingredient_id"
   end
 
   create_table "recipes", force: :cascade do |t|
