4ce3ead7cffaf7b8276c819e45fa416078ba31ca
Dan Bonehill <Danbonehill@me.com>
2017-05-24 19:37:57 +0000

    Add ingredient show page, add badge to recipes & add ingredient listing

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,10 +1,12 @@
 class IngredientsController < ApplicationController
+  before_action :set_ingredient, only: [:edit, :update, :show]
   
   def index
     @ingredients = Ingredient.paginate(page: params[:page], per_page: 5)
   end
   
   def show
+    @ingredient_recipes = @ingredient.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -20,6 +22,12 @@ class IngredientsController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:id])
   end
   
 end
